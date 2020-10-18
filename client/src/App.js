3fd03ace806d94237653dc5e73cc7d79a7d8f5af
paolaozv <paolaozv@gmail.com>
2020-10-18 18:40:55 +0200

    add MenuBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import MenuBar from './components/MenuBar';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi world!</h1>
-    </div>
-  );
+    return (
+      <Router>
+          <div className='ui container'>
+              <MenuBar />
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+          </div>
+      </Router>
+    );
 }
 
 export default App;
